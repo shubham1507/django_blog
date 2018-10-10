47b87002b93acaad9806a344c7204791b084849d
Shubham Joshi <shubham.joshi1507@gmail.com>
2018-10-11 00:05:34 +0530

    snj code kr raha hai

---
+++ mysite/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    path('admin/', admin.site.urls),
+]
